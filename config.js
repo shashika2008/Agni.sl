248508ad18fbe744a900df51bc4b28fbe86ec1d7
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-11 10:30:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XY9QQK7Y#rewRPcbc2ZfnrFt25jPo5vyLNf6pgI5dd9hwXD8N2oQ", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "aJRDhD7D#Io66l6JkyY_nJcR9Fm4-tctM873S7lfA2l5t0gb5aGE", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb://mongo:TIeFqQLptfPyhBsZDTHLLUANpZSVCDKg@interchange.proxy.rlwy.net:47301",  //ඔයාගෙ mongoDb url එක
 };
  
